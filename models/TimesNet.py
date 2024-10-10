dccabfbcdc21a90274551b1a4c9647e5c97c287d
zyb <1039102391@qq.com>
2024-10-10 14:41:16 +0800

    update param

--- models/TimesNet.py
+++ models/TimesNet.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch import Tensor
 import torch.nn.functional as F
 import torch.fft
 from layers.Embed import DataEmbedding
@@ -19,21 +20,31 @@ def FFT_for_Period(x, k=2):
 
 
 class TimesBlock(nn.Module):
-    def __init__(self, configs):
+    def __init__(self, seq_len: int, pred_len: int, 
+                 d_model: int, d_ff: int, 
+                 num_kernels: int, top_k: int,) -> None:
+        r"""Times Block
+
+        Args:
+            seq_len (int): the length of input sequence
+            pred_len (int): the length of prediction sequence
+            d_model (int): _description_
+            d_ff (int): _description_
+            num_kernels (int): _description_
+            top_k (int): _description_
+        """
         super(TimesBlock, self).__init__()
-        self.seq_len = configs.seq_len
-        self.pred_len = configs.pred_len
-        self.k = configs.top_k
+        self.seq_len = seq_len
+        self.pred_len = pred_len
+        self.k = top_k
         # parameter-efficient design
         self.conv = nn.Sequential(
-            Inception_Block_V1(configs.d_model, configs.d_ff,
-                               num_kernels=configs.num_kernels),
+            Inception_Block_V1(d_model, d_ff, num_kernels),
             nn.GELU(),
-            Inception_Block_V1(configs.d_ff, configs.d_model,
-                               num_kernels=configs.num_kernels)
+            Inception_Block_V1(d_ff, d_model, num_kernels)
         )
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         B, T, N = x.size()
         period_list, period_weight = FFT_for_Period(x, self.k)
 
@@ -42,16 +53,14 @@ class TimesBlock(nn.Module):
             period = period_list[i]
             # padding
             if (self.seq_len + self.pred_len) % period != 0:
-                length = (
-                                 ((self.seq_len + self.pred_len) // period) + 1) * period
+                length = (((self.seq_len + self.pred_len) // period) + 1) * period
                 padding = torch.zeros([x.shape[0], (length - (self.seq_len + self.pred_len)), x.shape[2]]).to(x.device)
                 out = torch.cat([x, padding], dim=1)
             else:
                 length = (self.seq_len + self.pred_len)
                 out = x
             # reshape
-            out = out.reshape(B, length // period, period,
-                              N).permute(0, 3, 1, 2).contiguous()
+            out = out.reshape(B, length // period, period, N).permute(0, 3, 1, 2).contiguous()
             # 2D conv: from 1d Variation to 2d Variation
             out = self.conv(out)
             # reshape back
@@ -73,36 +82,44 @@ class Model(nn.Module):
     Paper link: https://openreview.net/pdf?id=ju_Uqw384Oq
     """
 
-    def __init__(self, configs):
+    def __init__(self, configs,
+                 seq_len: int,
+                 e_layers: int,
+                 enc_in: int,
+                 d_model: int,
+                 d_ff: int,
+                 num_kernels: int,
+                 dropout: float,
+                 top_k: int = 2,
+                 ) -> None:
         super(Model, self).__init__()
-        self.configs = configs
+        # self.configs = configs
         self.task_name = configs.task_name
-        self.seq_len = configs.seq_len
+        self.seq_len = seq_len
+        pred_len = 0  # changed in forcasting task
         if self.task_name == 'long_term_forecast' or self.task_name == 'short_term_forecast':
             self.label_len = configs.label_len
             self.pred_len = configs.pred_len
-        self.model = nn.ModuleList([TimesBlock(configs) for _ in range(configs.e_layers)])
-        self.enc_embedding = DataEmbedding(configs.enc_in, 
-                                           configs.d_model, 
-                                           configs.embed, 
-                                           configs.freq,
-                                           configs.dropout)
+        self.model = nn.ModuleList([TimesBlock(seq_len=seq_len, pred_len=pred_len, d_model=d_model, 
+                                               d_ff=d_ff, num_kernels=num_kernels, top_k=top_k) 
+                                    for _ in range(e_layers)])
+        self.enc_embedding = DataEmbedding(enc_in, d_model, configs.embed, configs.freq, dropout)
         self.layer = configs.e_layers
-        self.layer_norm = nn.LayerNorm(configs.d_model)
+        self.layer_norm = nn.LayerNorm(d_model)
         
         if self.task_name == 'long_term_forecast' or self.task_name == 'short_term_forecast':
             self.predict_linear = nn.Linear(self.seq_len, self.pred_len + self.seq_len)
-            self.projection = nn.Linear(configs.d_model, configs.c_out, bias=True)
+            self.projection = nn.Linear(d_model, configs.c_out, bias=True)
             
         if self.task_name == 'imputation' or self.task_name == 'anomaly_detection':
-            self.projection = nn.Linear(configs.d_model, configs.c_out, bias=True)
+            self.projection = nn.Linear(d_model, configs.c_out, bias=True)
             
         if self.task_name == 'classification':
             self.act = F.gelu
-            self.dropout = nn.Dropout(configs.dropout)
-            self.projection = nn.Linear(configs.d_model * configs.seq_len, configs.num_class)
+            self.dropout = nn.Dropout(dropout)
+            self.projection = nn.Linear(d_model * seq_len, configs.num_class)
 
-    def forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
+    def forecast(self, x_enc: Tensor, x_mark_enc: Tensor) -> Tensor:
         # Normalization from Non-stationary Transformer
         means = x_enc.mean(1, keepdim=True).detach()
         x_enc = x_enc - means
@@ -201,18 +218,21 @@ class Model(nn.Module):
         output = self.projection(output)  # (batch_size, num_classes)
         return output
 
-    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
+    def forward(self, x_enc: Tensor, x_mark_enc: Tensor, 
+                x_dec: Tensor, x_mark_dec: Tensor, 
+                mask=None) -> Tensor:
         if self.task_name == 'long_term_forecast' or self.task_name == 'short_term_forecast':
-            dec_out = self.forecast(x_enc, x_mark_enc, x_dec, x_mark_dec)
+            dec_out = self.forecast(x_enc, x_mark_enc)
             return dec_out[:, -self.pred_len:, :]  # [B, L, D]
-        if self.task_name == 'imputation':
+        elif self.task_name == 'imputation':
             dec_out = self.imputation(
                 x_enc, x_mark_enc, x_dec, x_mark_dec, mask)
             return dec_out  # [B, L, D]
-        if self.task_name == 'anomaly_detection':
+        elif self.task_name == 'anomaly_detection':
             dec_out = self.anomaly_detection(x_enc)
             return dec_out  # [B, L, D]
-        if self.task_name == 'classification':
+        elif self.task_name == 'classification':
             dec_out = self.classification(x_enc, x_mark_enc)
             return dec_out  # [B, N]
-        return None
+        else:
+            raise NotImplementedError
